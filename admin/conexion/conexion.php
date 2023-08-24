e2dbfbcae7e5b075158f4cc6e30e825667943548
FerminNG <fermincopoboruloeri24@gmail.com>
2023-08-25 00:29:35 +0200

    accediendo al admin

--- admin/conexion/conexion.php
+++ admin/conexion/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conn= new mysqli("127.0.0.1","root", "","tesoreria");
+$conn= new mysqli("127.0.0.1","root", "","preteg2023");
 
 if($conn->connect_error){
     die("eror de conexion" . $conn->connect_error);

c4dc0ca97118bd8593f24f0dc81f5a511eff5abf
unknown <salvadormete2@gmail.com>
2023-08-25 02:03:06 +0200

    hola

--- admin/componentes/head.php
+++ admin/componentes/head.php
@@ -1,15 +1,15 @@
 <?php
 
-require '../conexion/conexion.php';
-session_start();
-if (!isset($_SESSION['usuario'])) {
+    // require '../conexion/conexion.php';
+    // session_start();
+    // if (!isset($_SESSION['usuario'])) {
 
-    header('Location:../index.php');
-}
+    //     header('Location:../index.php');
+    // }
 
-$usuario = $_SESSION['usuario'];
+    // $usuario = $_SESSION['usuario'];
 
-$usuario_id = $_SESSION['codigo'];
+    // $usuario_id = $_SESSION['codigo'];
 
 
 
